20808477e230c01629bc22bb2fc540a4b77c5aea
LORD <21MH1A0453@acoe.edu.in>
2023-10-17 22:21:15 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: firstscreen(),debugShowCheckedModeBanner: false,);
+  }
+}
+class firstscreen extends StatefulWidget {
+  const firstscreen({super.key});
+
+  @override
+  State<firstscreen> createState() => _firstscreenState();
+}
+
+class _firstscreenState extends State<firstscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('FireBase'),),
+
+    );
+  }
+}
+
